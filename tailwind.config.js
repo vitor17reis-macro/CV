a552c03cd74b83af83f5f7763f8d40194ab3277b
Vitor Reis <vitorreis@AirdeVitorReis.lan>
2025-08-26 23:38:53 +0100

    atualizei sobre mim e link do GitHub

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,5 +1,6 @@
 /** @type {import('tailwindcss').Config} */
 export default {
+  darkMode: 'class',   // 👈 esta linha é obrigatória
   content: ["./index.html", "./src/**/*.{js,jsx}"],
   theme: {
     extend: {},
