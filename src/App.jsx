e5df20cbcd6c72befba699697324bf2c8f992662
Vitor Reis <vitorreis@AirdeVitorReis.lan>
2025-08-31 19:41:49 +0100

    Adiciona artigo Portfolios2025

--- src/App.jsx
+++ src/App.jsx
@@ -1,18 +1,11 @@
-// src/App.jsx
-import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
-import EconomistaSiteBase from './EconomistaSiteBase';
-import DXY2025 from './pages/DXY2025';
-import Portfolios2025 from './pages/Portfolios2025';
+import React from "react";
+import EconomistaSiteBase from "./EconomistaSiteBase";
 
 function App() {
   return (
-    <Router>
-      <Routes>
-        <Route path="/" element={<EconomistaSiteBase />} />
-        <Route path="/analises/dxy-2025" element={<DXY2025 />} />
-        <Route path="/analises/portfolios-2025" element={<Portfolios2025 />} />
-      </Routes>
-    </Router>
+    <div>
+      <EconomistaSiteBase />
+    </div>
   );
 }
 
--- vite.config.js
+++ vite.config.js
@@ -1,6 +1,14 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+import { defineConfig } from 'vite';
+import react from '@vitejs/plugin-react';
 
 export default defineConfig({
   plugins: [react()],
-})
+  resolve: {
+    alias: {
+      '/pages': '/src/pages'
+    }
+  },
+  server: {
+    open: true
+  }
+});
