f50aae95944a657fa1291282d969d7c399673593
Vitor Reis <vitorreis@AirdeVitorReis.lan>
2025-08-31 19:45:14 +0100

    Adiciona artigo Portfolios2025

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,18 @@
-import React from "react";
-import EconomistaSiteBase from "./EconomistaSiteBase";
+// src/App.jsx
+import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
+import EconomistaSiteBase from './EconomistaSiteBase';
+import DXY2025 from './DXY2025';
+import Portfolios2025 from './Portfolios2025';
 
 function App() {
   return (
-    <div>
-      <EconomistaSiteBase />
-    </div>
+    <Router>
+      <Routes>
+        <Route path="/" element={<EconomistaSiteBase />} />
+        <Route path="/analises/dxy-2025" element={<DXY2025 />} />
+        <Route path="/analises/portfolios-2025" element={<Portfolios2025 />} />
+      </Routes>
+    </Router>
   );
 }
 
